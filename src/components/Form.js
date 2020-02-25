f1218b739e7e60e2a96806ceb571d390253546f8
axmonsalve <axel.monsalve.ponce@gmail.com>
2020-02-25 01:18:27 -0300

    Creating state for meeting form. Adding function for read inputs and destructuring from meeting state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Form = () => {
+  //Crear state de citas
+  const [meeting, setMeeting] = useState({
+    pet: "",
+    owner: "",
+    date: "",
+    time: "",
+    symptoms: ""
+  });
+
+  //Funcion que se ejecuta cuando el usuario escribe en un input
+  const handleChange = e => {
+    //Funcion con el objeto para llenar el state
+    setMeeting({
+      //Tomamos una copia del state
+      ...meeting,
+      //Escribimos en el campo seleccionado
+      [e.target.name]: e.target.value
+    });
+  };
+
+  //Destructuring del state (objeto)
+  const { pet, owner, date, time, symptoms } = meeting;
   return (
     <Fragment>
       <h2>Crear Cita</h2>
@@ -9,28 +31,51 @@ const Form = () => {
         <input
           type="text"
           name="pet"
-          id=""
           className="u-full-width"
           placeholder="Nombre de la mascota"
+          onChange={handleChange}
+          value="pet"
         />
         <label>Nombre del Dueño</label>
         <input
           type="text"
           name="owner"
-          id=""
           className="u-full-width"
           placeholder="Nombre del dueño"
+          onChange={handleChange}
+          value="owner"
         />
         <label>Fecha</label>
-        <input type="date" name="date" id="" className="u-full-width" />
+        <input
+          type="date"
+          name="date"
+          className="u-full-width"
+          onChange={handleChange}
+          value="date"
+        />
         <label>Hora</label>
-        <input type="time" name="time" id="" className="u-full-width" />
+        <input
+          type="time"
+          name="time"
+          className="u-full-width"
+          onChange={handleChange}
+          value="time"
+        />
         <label>Síntomas</label>
-        <textarea className="u-full-width"></textarea>
+        <textarea
+          className="u-full-width"
+          name="symptoms"
+          onChange={handleChange}
+          value="symptoms"
+        ></textarea>
+        
         <button
-            type="button"
-            className="u-full-width button-primary"
-        >Agregar Cita</button>
+          type="button"
+          name="symptoms"
+          className="u-full-width button-primary"
+        >
+          Agregar Cita
+        </button>
       </form>
     </Fragment>
   );
