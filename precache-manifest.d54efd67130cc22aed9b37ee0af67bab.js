19d782b5236da15704cc21d9cc89d991ecfd9f6e
axmonsalve <axel.monsalve.ponce@gmail.com>
2020-02-26 18:43:34 -0300

    Updates

---
+++ precache-manifest.d54efd67130cc22aed9b37ee0af67bab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e73abf6dcb6bab7eb67a31ae5ec7ffa",
+    "url": "/vet/index.html"
+  },
+  {
+    "revision": "17cc11515ff8e178ea93",
+    "url": "/vet/static/css/main.fc8ec401.chunk.css"
+  },
+  {
+    "revision": "7a6f8eb3b59e94c44b0c",
+    "url": "/vet/static/js/2.3a9b52db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/vet/static/js/2.3a9b52db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17cc11515ff8e178ea93",
+    "url": "/vet/static/js/main.fd2d0021.chunk.js"
+  },
+  {
+    "revision": "6e49500cde9dc86e84c3",
+    "url": "/vet/static/js/runtime-main.22fb339c.js"
+  }
+]);
